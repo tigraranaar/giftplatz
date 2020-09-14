ccebd2909171c88e082a37e2c284bb801dd2d723
tigraranaar <Tigran1402_3991>
2020-09-14 17:40:10 +0400

    ll

--- src/modules/main.js
+++ src/modules/main.js
@@ -3,6 +3,7 @@ import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 import '../favicons/favicons';
 import '../modules/resize';
+import '../modules/lazy';
 import '../modules/subscribe';
 import '../scss/custom.scss';
 import 'bootstrap/scss/bootstrap.scss';
